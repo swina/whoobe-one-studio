e61a4970ea8fb5069f7c8ae76218ce9018a9e811
Swina Allen <swina.allen@gmail.com>
2021-10-15 00:03:33 +0300

    Various major fixes

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6129a375cdb3243eaf4b83609c6fdf79.js"
+  "/precache-manifest.1783529350105e5386fa3377ba35d25f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "whoobe-studio"});
